f3f17f39f95e24f73a5e149ff09f2d0f6acf4830
darthVarada <64443178+darthVarada@users.noreply.github.com>
2024-02-21 00:29:46 -0300

    front para streamlit & Power BI embed

---
+++ analise.py
@@ -0,0 +1,7 @@
+
+import streamlit as st
+
+
+st.text("bets")
+#st.dataframe(resultados)
+st.markdown('<iframe width="800" height="600" src="https://drive.google.com/file/d/1-1mfUrbQna3HnGtB_CJtQ04IbCpSvXeH/view?usp=sharing"></iframe>', unsafe_allow_html=True)
